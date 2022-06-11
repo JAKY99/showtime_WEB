78da6e0dbcb83a39a9e1fc71bbd912b6e8a252a2
Achot BARSEGHYAN <achot.barseghyan@gmail.com>
2022-06-11 15:40:33 +0200

    feat: framework primeNG install instead of material ui and menubar component created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {SidebarModule} from "primeng/sidebar";
+import {ButtonModule} from "primeng/button";
+import { MenubarComponent } from './menubar/menubar.component';
+import {MenubarModule} from "primeng/menubar";
+import {InputTextModule} from "primeng/inputtext";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenubarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    SidebarModule,
+    ButtonModule,
+    MenubarModule,
+    InputTextModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
