ae2d5623f8f879da5fc4ac28720e2a4a73f4ca0e
Achot BARSEGHYAN <achot.barseghyan@gmail.com>
2022-08-07 02:17:16 +0200

    update: breadcrumb getLabel function

--- src/app/services/breadcrumb/breadcrumb.service.ts
+++ src/app/services/breadcrumb/breadcrumb.service.ts
@@ -45,7 +45,7 @@ export class BreadcrumbService {
           };
         }else{
           breadcrumb = {
-            label: this.getLabel(route.data),
+            label: BreadcrumbService.getLabel(route.data),
             routerLink: '/' + routeUrl.join('/')
           };
         }
@@ -56,8 +56,7 @@ export class BreadcrumbService {
     }
   }
 
-  private getLabel(data: Data) {
-    // The breadcrumb can be defined as a static string or as a function to construct the breadcrumb element out of the route data
+  private static getLabel(data: Data) {
     return typeof data.breadcrumb === 'function' ? data.breadcrumb(data) : data.breadcrumb;
   }
 }
